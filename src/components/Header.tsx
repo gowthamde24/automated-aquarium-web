5d0a85c02fb890ec03938bb6fbab1d9fbd8bdaa6
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-03-05 20:16:26 +0100

    Fix: Comment not ready files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,16 +38,16 @@ export default function Header() {
                 Streaming
               </Link>
             </li> */}
-            <li>
+            {/* <li>
               <Link href="/blog" className="hover:text-gray-300">
                 Blog
               </Link>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <Link href="/about" className="hover:text-gray-300">
                 About
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link href="/contact" className="hover:text-gray-300">
                 Contact
