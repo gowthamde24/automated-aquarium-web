7dcc52dd82aa2bdb0bbb35f73e5d8cfba93c1460
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-03-14 15:14:30 +0100

    Feat: Add about page

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+};
+
 export default function AboutLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <div className="bg-gray-900 font-[family-name:var(--font-geist-sans)]">{children}</div>
+  return (
+    <div className="bg-gray-900 font-[family-name:var(--font-geist-sans)]">
+      {children}
+    </div>
+  );
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,18 @@
 export default function About() {
- 
   return (
-    <div className="flex grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 pt-32 ml-16 mr-16">
-      
+    <div className="flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20 pt-32 ml-16 mr-16">
+      <div className="bg-[rgb(42,52,73)] w-full h-64 flex items-center justify-center mb-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Automated Aquarium Project</h1>
+          <p className="text-lg">This project aims to automate the maintenance and monitoring of aquariums using modern technologies.</p>
+        </div>
+      </div>
+      <div className="bg-[rgb(42,52,73)] w-full h-64 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">About Me</h1>
+          <p className="text-lg">I am a passionate developer with a keen interest in automation and IoT projects. This project is a testament to my dedication and skills.</p>
+        </div>
+      </div>
     </div>
   );
 }
