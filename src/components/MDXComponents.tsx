ae531812b98f25b742d1eb17b62810f2c851dc97
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-04-29 22:45:23 +0200

    Feat(Markdown): Format Markdown

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -22,9 +22,9 @@ export const components: MDXComponents = {
   ul: (props) => <ul className="list-disc pl-6 my-4" {...props} />,
   ol: (props) => <ol className="list-decimal pl-6 my-4" {...props} />,
   li: (props) => <li className="my-1" {...props} />,
-  blockquote: (props) => <blockquote className="border-l-4 border-gray-300 pl-4 italic my-4" {...props} />,
+  blockquote: (props) => <blockquote className="border-l-4 border-gray-500 pl-4 italic my-4" {...props} />,
   hr: () => <hr className="my-6" />,
   strong: (props) => <strong className="font-bold" {...props} />,
   em: (props) => <em className="italic" {...props} />,
-  code: (props) => <code className="bg-gray-100 rounded px-1 py-0.5" {...props} />,
+  code: (props) => <code className="bg-gray-500 rounded px-1 py-0.5" {...props} />,
 }
