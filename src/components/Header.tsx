fbdcfca971656f05acdec317318d2b1c7973a4f5
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-04-30 18:16:08 +0200

    Fix(Header): Close menu onclick

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,7 +105,7 @@ export default function Header() {
               </Link>
             </li> */}
             <li>
-              <Link href="/blog" className="hover:text-gray-300">
+              <Link href="/blog" className="hover:text-gray-300" onClick={handleNavItemClick}>
                 Blog
               </Link>
             </li>
