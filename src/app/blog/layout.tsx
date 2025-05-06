c8cd27bf58a6d257a8f430c545e1117c71824ffb
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-05-06 19:25:54 +0200

    Fix(Blog): Add layout

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+};
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-gray-900 font-[family-name:var(--font-geist-sans)]">
+      {children}
+    </div>
+  );
+}
